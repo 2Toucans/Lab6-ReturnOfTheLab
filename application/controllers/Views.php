addbea60d69b7e22b2935c197e59836760a48bd0
Aaron Freytag <aaroncfreytag@gmail.com>
2017-02-17 11:11:20 -0800

    Added priority-sorted view

--- application/controllers/Views.php
+++ application/controllers/Views.php
@@ -9,9 +9,45 @@ class Views extends Application
         $this->data['pagetitle'] = 'Ordered TODO List';
         $tasks = $this->tasks->all();
         $this->data['content'] = 'Ok';
-        $this->data['leftside'] = 'by_priority';
-        $this->data['rightside'] = 'by_category';
+        $this->data['leftside'] = $this->makePrioritizedPanel($tasks);
+        $this->data['rightside'] = $this->makeCategorizedPanel($tasks);
         
         $this->render('template_secondary');
     }
+    
+    private function makePrioritizedPanel($tasks) {
+        // extract the undone tasks
+        foreach ($tasks as $task)
+        {
+            if ($task->status != 2)
+                $undone[] = $task;
+        }
+        // order them by priority
+        usort($undone, "orderByPriority");
+        
+        // substitute the priority name
+        foreach ($undone as $task)
+            $task->priority = $this->priorities->get($task->priority)->name;
+            
+        // convert the array of task objects into an array of associative objects       
+        foreach ($undone as $task)
+            $converted[] = (array) $task;
+    
+        // and then pass them on
+        $parms = ['display_tasks' => $converted];
+        return $this->parser->parse('by_priority', $parms, true);
+    }
+    
+    private function makeCategorizedPanel($tasks) {
+    }
+}
+
+function orderByPriority($a, $b)
+{
+    if ($a->priority > $b->priority)
+        return -1;
+    elseif ($a->priority < $b->priority)
+        return 1;
+    else
+        return 0;
 }
